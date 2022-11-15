ea9ef59b8d0b6be9e988b648219a97b5ec13a97f
oljungde <oljung@posteo.de>
2022-11-15 19:28:00 +0100

    prepare login function

--- src/js/login.js
+++ src/js/login.js
@@ -1,10 +1,35 @@
 function userLogin() {
-    let userEmail = document.getElementById('user_email');
-    let userPassword = document.getElementById('user_password');
-
+    let userEmail = document.getElementById('user_email').value;
+    let userPassword = document.getElementById('user_password').value;
+    checkUserData(userEmail, userPassword)
 }
 
 
-function checkUserData() {
+function checkUserData(userEmail, userPassword) {
+    let indexOfEmail = users.findIndex(user => user.email == userEmail);
+    let indexOfPassword = users.findIndex(user => user.password == userPassword);
+    console.log(indexOfEmail);
+    console.log(indexOfPassword);
+    if (indexOfEmail == -1 || indexOfPassword == -1) {
+        console.log('Einloggen nicht möglich!');
+        // let loginContent = document.getElementById('login_content');
+        // loginContent.innerHTML = accessDeniedTemplate();
+    } else {
+        console.log('GLÜCKWUNSCH! Einloggen möglich');
+    }
+}
 
+
+function accessDeniedTemplate() {
+    return /*html*/ `
+        <a href="./index.html"><img src="./assets/img/arrow-back.png" alt="back to login" class="signup-back"></a>
+        <div class="headline-container">
+            <h1 class="headline">Access Denied</h1>
+            <div class="login-headline-border"></div>
+        </div>
+
+        <div class="access-denied-text">
+            Sorry, either you entered a wrong email address or password.<br> You can try again <a href="./index.html">here</a>.
+        </div>
+    `;
 }
