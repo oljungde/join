34b26412094fdf47b2b2a32f110b7426d1b7624a
Patrick Kolodziej <116544154+PatrickKolo@users.noreply.github.com>
2022-11-18 07:21:29 +0100

    worked on navigation bar highlight feature

--- src/js/board.js
+++ src/js/board.js
@@ -174,3 +174,5 @@ function closeDetailTask() {
     updateHTML()
 
 }
+
+
--- src/js/script.js
+++ src/js/script.js
@@ -22,3 +22,30 @@ async function includeHTML() {
         }
     }
 }
+
+// defining which link was clicked and giving the function BgSelectedNav the matching id
+
+function changeNavbarBgColor(bar) {
+
+    if (bar == 1) {
+        BgSelectedNav('navigation_summary');  
+    }
+    if (bar == 2) {
+        BgSelectedNav('navigation_board');
+    }
+    if (bar == 3) {
+        BgSelectedNav('navigation_addTask');  
+    }
+    if (bar == 4) {
+        BgSelectedNav('navigation_contacts');   
+    }
+    if (bar == 5) {
+        BgSelectedNav('navigation_legal');
+    }
+}
+
+// adding class bg-blue to the selected link
+
+function BgSelectedNav(element) {
+    document.getElementById(`${element}`).classList.add('bg-blue');
+}
