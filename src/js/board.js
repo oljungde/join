4ba1f8d9251cd9421c3f1144091d980325f55005
Patrick Kolodziej <116544154+PatrickKolo@users.noreply.github.com>
2023-01-03 15:38:51 +0100

    add-task Site design

--- src/js/board.js
+++ src/js/board.js
@@ -538,8 +538,8 @@ function closeDetailTask() {
  */
 function hideAddTaskMask() {
     setTimeout(() => {
-    //    document.getElementById("AddTaskMaskBg").classList.add("d-none");
-    //   document.getElementById("detail_container").classList.add("d-none");
+        document.getElementById("AddTaskMaskBg").classList.add("d-none");
+       document.getElementById("detail_container").classList.add("d-none");
 
     }, 250);
   }
