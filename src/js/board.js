b0a77c8cd03fc43dd8d86d4014e2290ce7a5b616
Patrick Kolodziej <116544154+PatrickKolo@users.noreply.github.com>
2023-01-04 14:06:59 +0100

    close windows with overlay click

--- src/js/board.js
+++ src/js/board.js
@@ -525,6 +525,7 @@ function hideAddTaskMask() {
     setTimeout(() => {
         document.getElementById("AddTaskMaskBg").classList.add("d-none");
         document.getElementById("detail_container").classList.add("d-none");
+        document.getElementById('AddTaskMaskContact').classList.add("d-none");
 
     }, 250);
 }
